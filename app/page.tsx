98244bd6797009bd482097b9f74e95136e2bd022
nckenn <nckenn.developer@gmail.com>
2023-05-27 19:40:28 +0800

    refactor: adjust spacing of landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import Auth0SignInButton  from '@/components/auth0-signin-button';
 export default function Home() {
 
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 ">
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 max-w-4xl mt-12">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
           Welcome to the weather forecast web application. Please login with your Github user to
           use the application and view the weather in your city.
         </p>
       </div>
-      <div className="flex gap-4">
+      <div className="flex gap-4 mt-4">
         <Auth0SignInButton></Auth0SignInButton>
       </div>
     </section>
