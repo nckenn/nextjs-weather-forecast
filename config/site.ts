9ca3d49c8fe7053b0a4e9dd002d85747ecae2ae3
nckenn <nckenn.developer@gmail.com>
2023-05-27 13:43:03 +0800

    feat: added shadcn UI as a main UI Component

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Weather Forecast",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/nckenn",
+    github: "https://github.com/nckenn/nextjs-weather-forecast"
+  },
+}
